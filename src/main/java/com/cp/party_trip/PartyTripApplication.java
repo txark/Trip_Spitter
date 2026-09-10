f3d434bf226893d9cc7306a2f7f61f7a65ee555b
Txark <artitaya.tk@gmail.com>
2026-09-10 21:03:55 +0700

    first commit

---
+++ src/main/java/com/cp/party_trip/PartyTripApplication.java
@@ -0,0 +1,13 @@
+package com.cp.party_trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cp/party_trip/PartyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.party_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
